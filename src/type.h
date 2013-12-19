c247f451d836fc2f2b0f684d05207ed0f1066a29
HuaguanChen <chenhuaguanzju@gmail.com>
2013-12-19 18:44:10 +0800

    [FIX] fix the type size problem.

--- src/type.h
+++ src/type.h
@@ -1,6 +1,9 @@
 #ifndef TYPE_H_2013_11_8
 #define TYPE_H_2013_11_8
 
+#include <QtGlobal>
+
+/*
 typedef signed char s8;
 typedef unsigned char u8;
 typedef signed short s16;
@@ -9,6 +12,18 @@ typedef signed int s32;
 typedef unsigned int u32;
 typedef signed long long s64;
 typedef unsigned long long u64;
+
+typedef s64 s48;
+typedef u64 u48;
+*/
+typedef qint8 s8;
+typedef quint8 u8;
+typedef qint16 s16;
+typedef quint16 u16;
+typedef qint32 s32;
+typedef quint32 u32;
+typedef qint64 s64;
+typedef quint64 u64;
 
 typedef s64 s48;
 typedef u64 u48;
